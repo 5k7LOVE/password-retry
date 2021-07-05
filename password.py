35b06a861f1a7901312a1f5e5e418c531b4af9ea
翁克齊 <bob19870825@gmail.com>
2021-07-05 17:30:46 +0800

    basic version done

---
+++ password.py
@@ -0,0 +1,40 @@
+# ＃密碼重是程式
+# password = 'a123456'
+# 讓使用者重複輸入密碼
+# 最多輸入3次
+# 如果正確就印出'登入成功'
+# 如果不正確就印出'密碼錯誤！'還有＿次機會！
+
+password = 'a123456'
+n = 3
+# kayIn = input('請輸入密碼')
+
+# while n >= 1:
+# 	if kayIn != password:
+# 		print('密碼錯誤！還有', n, '次機會！')
+# 		n -= 1
+# 		kayIn = input('請輸入密碼')
+# 		if kayIn != password and n == 0:
+# 			print('帳號被鎖定定定定定')
+# 		elif kayIn == password:
+# 			print('登入成功')
+# 			break
+# 	else:
+# 		print('登入成功')
+# 		break
+
+while True:
+	kayIn = input('請輸入密碼')
+	if kayIn == password:
+		print('登入成功')
+		break
+	else:
+		n -= 1
+		print('密碼錯誤！還有', n, '次機會！')
+		if n == 0:
+			print('帳號被鎖定定定定定')
+			break
+
+
+
+
